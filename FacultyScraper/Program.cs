1c0ac1297f5a7a0eeae4ab698e0f78027a5bf745
Information School <ischoolit@uw.edu>
2015-04-22 18:32:11 -0700

    started law school

--- FacultyScraper/Program.cs
+++ FacultyScraper/Program.cs
@@ -1,6 +1,7 @@
 ﻿using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,12 @@ namespace FacultyScraper
     {
         public class Faculty
         {
-            private string name, university, department, phd;
+            private string fname, lname, university, department, phd;
 
-            public Faculty(string n, string u, string d, string p)
+            public Faculty(string n, string l, string u, string d, string p)
             {
-                name = n;
+                fname = n;
+                lname = l;
                 university = u;
                 department = d;
                 phd = p;
@@ -23,40 +25,66 @@ namespace FacultyScraper
 
             public string toString()
             {
-                return name + ", " + university + ", " + department + ", " + phd;
+                return fname + ", " + lname + ", " + university + ", " + department + ", " + phd;
             }
 
         }
 
         static void Main(string[] args)
         {
-            List<String> profiles;
-            profiles = getProfiles("https://www.psych.ucla.edu/faculty");
-
             List<Faculty> facultyList = new List<Faculty>();
-            foreach (String profile in profiles)
+
+            List<String> psyProfiles;
+            psyProfiles = getPsyProfiles("https://www.psych.ucla.edu/faculty");
+
+            List<String> lawProfiles;
+            lawProfiles = getLawProfiles("https://law.ucla.edu/faculty/faculty-profiles/");
+            foreach (String profile in lawProfiles)
+            {
+                Console.WriteLine(profile);
+            }
+
+            foreach (String profile in psyProfiles)
             {
                 Faculty prof;
-                prof = extractData(profile);
+                prof = extractPsyData(profile);
                 if (prof != null)
                 {
                     facultyList.Add(prof);
                 }
             }
+            //--------------------------------------------------------------------
 
-            foreach (var faculty in facultyList)
-            {
-                Console.WriteLine(faculty.toString());
-            }
+            
+
+            exportCsv(facultyList);
+            
+
+            
 
             
         }
 
+
+
+        public static void exportCsv(List<Faculty> facultyList)
+        {
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\iguest\Documents\Visual Studio 2013\Projects\FacultyScraper\faculty.csv"))
+            {
+                foreach (var faculty in facultyList)
+                {
+                    //Console.WriteLine(faculty.toString());
+                    sw.WriteLine(faculty.toString());
+                }
+            }
+        }
+
         // collects all necessary fields from each faculty profile and returns a faculty object
-        public static Faculty extractData(string url)
+        public static Faculty extractPsyData(string url)
         {
             Faculty prof = null;
             string n = "n/a";
+            string l = "n/a";
             string u = "UCLA";
             string d = "Psychology";
             string p = "n/a";
@@ -74,8 +102,19 @@ namespace FacultyScraper
                         string attribute = tag.Attributes["class"].Value;
                         if (attribute.Contains("name"))
                         {
-                            string name = tag.InnerText;
-                            n = name.Trim();
+                            string name = tag.InnerText.Trim();
+                            string[] words = name.Split(' ');
+                            if (words.Length > 2)
+                            {
+                                n = words[0];
+                                l = words[2];
+                            }
+                            else
+                            {
+                                n = words[0];
+                                l = words[1];
+                            }
+                            
                         }
                         else if (attribute.Contains("phd"))
                         {
@@ -84,7 +123,7 @@ namespace FacultyScraper
                             p = words[1].Trim();
                             //Console.WriteLine(n);
                             //Console.WriteLine(p);
-                            prof = new Faculty(n, u, d, p);
+                            prof = new Faculty(n, l, u, d, p);
                         }
                     }
                 }
@@ -92,8 +131,8 @@ namespace FacultyScraper
             return prof;
         }
 
-        //opens root url and gets urls for each faculty's personal web page
-        public static List<String> getProfiles(string address)
+        //opens ucla psychology root url and gets urls for each faculty's personal web page
+        public static List<String> getPsyProfiles(string address)
         {
             //sets the target url
             HtmlWeb hw = new HtmlWeb();
@@ -119,5 +158,31 @@ namespace FacultyScraper
             }
             return profiles;
         }
+
+        public static List<String> getLawProfiles(string address)
+        {
+            //sets the target url
+            HtmlWeb hw = new HtmlWeb(); 
+            HtmlDocument doc = hw.Load(address);
+            List<String> profiles = new List<String>();
+
+            var aTags = doc.DocumentNode.SelectNodes("//a");
+            if (aTags != null)
+            {
+                foreach (var tag in aTags)
+                {
+                    if (tag.Attributes["href"] != null)
+                    {
+                        string newAddress = "https://law.ucla.edu" + tag.Attributes["href"].Value;
+                        if (newAddress.Contains("faculty-profiles/") && !profiles.Contains(newAddress))
+                        {
+                            profiles.Add(newAddress);
+                        }
+                    }
+                }
+            }
+            return profiles;
+        }
+
     }
 }
